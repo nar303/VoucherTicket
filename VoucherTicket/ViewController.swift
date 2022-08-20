fa7b572a6acafbd926f3b83f9358789a7c4c4bb2
Narek <narek.ghukasyan303@gmail.com>
2022-08-21 00:26:05 +0400

    Initial Commit

---
+++ VoucherTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoucherTicket
+//
+//  Created by Narek Ghukasyan on 21.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
